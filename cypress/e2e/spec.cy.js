78a4356b88ad5ffbac3cdf7862b7c6f1fffcb337
Ayush Sachdeva <ayush.sachdeva@pipedrive.com>
2025-10-27 12:03:43 +0200

    Add example

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,15 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+
+  it('opens test HTML file with cy.origin', () => {
+    const baseUrl = Cypress.config('baseUrl') || 'http://localhost:8080'
+
+     cy.visit('https://example.cypress.io')
+
+    cy.origin(baseUrl, () => {
+      cy.visit('/')
+      cy.get('title').should('contain', 'Test Page')
+      cy.get('#testBtn').should('be.visible').and('contain', 'Click Me')
+    })
   })
 })
